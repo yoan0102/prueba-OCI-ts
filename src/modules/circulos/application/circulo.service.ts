c4c0aef800b310d8305f2a802ef66874e4a48346
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-10-28 18:30:36 -0400

    Fix: mejoras en los imports

--- src/modules/circulos/application/circulo.service.ts
+++ src/modules/circulos/application/circulo.service.ts
@@ -1,24 +1,23 @@
-import { ICirculo } from "../domain/circulo";
-import Circulo from "../infraestructure/datasource/Circulo.model";
+import { ICirculo } from '../domain/circulo';
+import Circulo from '../infraestructure/datasource/Circulo.model';
 
 export const getCirculoAll = async () => {
-  const query = { status: 1 };
-  return await Circulo.find(query);
+	const query = { status: 1 };
+	return await Circulo.find(query);
 };
 
-export const getCirculoById = async (id:string) => {
-  return await Circulo.findById(id);
+export const getCirculoById = async (id: string) => {
+	return await Circulo.findById(id);
 };
 
-
-export const createCirculo = async (entity:ICirculo) => {
-  return await Circulo.create(entity);
+export const createCirculo = async (entity: ICirculo) => {
+	return await Circulo.create(entity);
 };
 
-export const updateCirculoById = async (id:string, entity:ICirculo) => {
-  return await Circulo.findByIdAndUpdate(id, entity, { new: true });
+export const updateCirculoById = async (id: string, entity: ICirculo) => {
+	return await Circulo.findByIdAndUpdate(id, entity, { new: true });
 };
 
 export const removeCirculoById = async (id: string) => {
-  return await Circulo.findByIdAndUpdate(id, { status: 0 }, { new: true });
+	return await Circulo.findByIdAndUpdate(id, { status: 0 }, { new: true });
 };
--- src/shared/models/Server.ts
+++ src/shared/models/Server.ts
@@ -1,50 +1,52 @@
-import express, { Application } from 'express'
-import cors from 'cors'
+import express, { Application } from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
+import loger from 'morgan';
 
 import { connectDB } from '../../config/mongodb';
 import userRouter from '../../modules/users/infraestructure/routes/user.routes';
 import authRouter from '../../modules/users/infraestructure/routes/auth.routes';
-import circuloRouter from '../../modules/circulos/infraestructure/routes/CIRCULO.routes';
+import circuloRouter from '../../modules/circulos/infraestructure/routes/circulo.routes';
 
-export default class Server{
-  private app: Application;
-  private port: string;
-  private apiPath = {
-      users: '/api/users',
-      auth: '/api/auth',
-      circulo: '/api/circulo'
-  }
+export default class Server {
+	private app: Application;
+	private port: string;
+	private apiPath = {
+		users: '/api/users',
+		auth: '/api/auth',
+		circulo: '/api/circulo',
+	};
 
-  constructor(){
-    this.app = express();   
-    this.port = process.env.PORT || '8000';
-    
-    this.middlewares();
-    this.routes();
-    this.connectBD();
-  }
+	constructor() {
+		this.app = express();
+		this.port = process.env.PORT || '8000';
 
-  connectBD() {
-    connectDB();
-  }
-  middlewares() {
-    this.app.use(cors());
-    this.app.use(helmet());
-    this.app.use(express.json());
+		this.middlewares();
+		this.routes();
+		this.connectBD();
+	}
 
-    this.app.use(express.static('public'))
-  }
+	connectBD() {
+		connectDB();
+	}
+	middlewares() {
+		this.app.use(cors());
+		this.app.use(helmet());
+		this.app.use(loger('dev'));
+		this.app.use(express.json());
 
-  routes() {
-    this.app.use(this.apiPath.users, userRouter);
-    this.app.use(this.apiPath.auth, authRouter);
-    this.app.use(this.apiPath.circulo, circuloRouter);
-  }
+		this.app.use(express.static('public'));
+	}
 
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log(`Server runing on port ${this.port} 🚀🚀🚀`);     
-    })
-  }
+	routes() {
+		this.app.use(this.apiPath.users, userRouter);
+		this.app.use(this.apiPath.auth, authRouter);
+		this.app.use(this.apiPath.circulo, circuloRouter);
+	}
+
+	listen() {
+		this.app.listen(this.port, () => {
+			console.log(`Server runing on port ${this.port} 🚀🚀🚀`);
+		});
+	}
 }
