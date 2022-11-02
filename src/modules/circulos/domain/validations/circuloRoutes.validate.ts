53949f04ce1400de92389b5956616c213efea3d6
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-11-02 14:22:30 -0400

    fi : reafactoring createRoutes circulo

--- src/modules/circulos/domain/validations/circuloRoutes.validate.ts
+++ src/modules/circulos/domain/validations/circuloRoutes.validate.ts
@@ -21,6 +21,7 @@ export const validateRouteCreateCirculo = [
 	// isAdminRole
 	check('no').isNumeric().withMessage('El no es de tipo numerico').notEmpty().withMessage('El no es obligatorio'),
 	check('name', 'EL nombre es obligatorio').notEmpty(),
+	//capcacidad
 	check('capacidad_total')
 		.isNumeric()
 		.withMessage('La capacidad total es de tipo numerico')
@@ -46,6 +47,7 @@ export const validateRouteCreateCirculo = [
 		.withMessage('La capacidad de 5to ano es de tipo numerico')
 		.notEmpty()
 		.withMessage('La capacidad de 5to ano es obligatoria'),
+	//Matricula
 	check('matricula_total')
 		.isNumeric()
 		.withMessage('La matricula total ano es de tipo numerico')
@@ -53,7 +55,7 @@ export const validateRouteCreateCirculo = [
 		.withMessage('La matricula total es obligatoria'),
 	check('matricula2', 'La matricula de 2do ano es obligatoria')
 		.isNumeric()
-		.withMessage('La matricula de 2do ano ano es de tipo numerico')
+		.withMessage('La matricula de 2do  ano es de tipo numerico')
 		.notEmpty()
 		.withMessage('La matricula de 2do ano es obligatoria'),
 	check('matricula3')
@@ -63,14 +65,35 @@ export const validateRouteCreateCirculo = [
 		.withMessage('La matricula de 3ro ano es obligatoria'),
 	check('matricula4', 'La matricula de 4to ano es obligatoria')
 		.isNumeric()
-		.withMessage('La matricula de 4to ano ano es de tipo numerico')
+		.withMessage('La matricula de 4to ano es de tipo numerico')
 		.notEmpty()
 		.withMessage('La matricula de 4to ano es obligatoria'),
 	check('matricula5')
 		.isNumeric()
-		.withMessage('La matricula de 5to ano ano es de tipo numerico')
+		.withMessage('La matricula de 5to ano  es de tipo numerico')
 		.notEmpty()
 		.withMessage('La matricula de 5to ano es obligatoria'),
+	//asistencia
+	check('porciento_asistencia2')
+		.isNumeric()
+		.withMessage('El  porciento asistencia 2do ano es de tipo numerico')
+		.notEmpty()
+		.withMessage('El  porciento asistencia 2do ano es obligatoria'),
+	check('matricula3')
+		.isNumeric()
+		.withMessage('El  porciento asistencia 3ro ano es de tipo numerico')
+		.notEmpty()
+		.withMessage('El  porciento asistencia 3ro ano es obligatoria'),
+	check('matricula4')
+		.isNumeric()
+		.withMessage('El  porciento asistencia 4to ano es de tipo numerico')
+		.notEmpty()
+		.withMessage('El  porciento asistencia 4to ano es obligatoria'),
+	check('matricula5')
+		.isNumeric()
+		.withMessage('El  porciento asistencia 5to ano es de tipo numerico')
+		.notEmpty()
+		.withMessage('El  porciento asistencia 5to ano es obligatoria'),
 	// check('id').custom(issetCirculoId),
 	validateResultError,
 ];
