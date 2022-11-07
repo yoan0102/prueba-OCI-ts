3f86aca89ffba64eb457e2abe94fbd61f771d4b7
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-11-07 14:58:43 -0500

    fix: adding populate

--- src/modules/children/application/children.service.ts
+++ src/modules/children/application/children.service.ts
@@ -1,14 +1,14 @@
 import { IChildren } from '../domain/children';
 import Children from '../infraestructure/datasource/Children.model';
-import { radianesConvert } from 'shared/utils/radianesConvert';
+import { radianesConvert } from '../../../shared/utils/radianesConvert';
 
 export const getChildrenAll = async () => {
 	const query = { status: 1 };
-	return await Children.find(query);
+	return await Children.find(query).populate({ path: 'circulo', select: 'name' });
 };
 
 export const getChildrenById = async (id: string) => {
-	return await Children.findById(id);
+	return await Children.findById(id).populate({ path: 'circulo', select: 'name' });
 };
 
 export const createChildren = async (entity: IChildren) => {
@@ -22,7 +22,7 @@ export const createChildren = async (entity: IChildren) => {
 };
 
 export const updateChildrenById = async (id: string, entity: IChildren) => {
-	return await Children.findByIdAndUpdate(id, entity, { new: true });
+	return await Children.findByIdAndUpdate(id, entity, { new: true }).populate({ path: 'circulo', select: 'name' });
 };
 
 export const removeChildrenById = async (id: string) => {
