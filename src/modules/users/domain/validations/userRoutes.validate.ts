a0b2640da5c7c4291c7e215d9be5fa8195c0f60e
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-11-07 13:08:46 -0500

    fix: refactoring   router validation

--- src/modules/users/domain/validations/userRoutes.validate.ts
+++ src/modules/users/domain/validations/userRoutes.validate.ts
@@ -28,6 +28,12 @@ export const validateRouteUpdateUser = [
 	// jwtValid,
 	// isAdminRole,
 	check('id', 'ID no valido').isMongoId().custom(issetUserId),
+	check('nick_name').not().custom(issetNickName).withMessage('El usuario no existe'),
+	check('first_name', 'EL nombre es obligatorio').notEmpty(),
+	check('last_name', 'EL apellido es obligatorio').notEmpty(),
+	check('password', 'La contraseña es obligatoria y debe tener más de 5 caracteres').isLength({ min: 6 }),
+	check('position', 'El cargo que ocupa en su empresa es obligatorio').notEmpty(),
+	check('role').isIn(['ADMIN_ROLE', 'USER_ROLE']).withMessage('No es un role permitido').custom(issetRoleValido),
 	validateResultError,
 ];
 
