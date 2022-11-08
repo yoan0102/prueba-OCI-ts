f64c974469ac21dd1be441ce38a0b09494c38cae
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-11-08 11:25:46 -0500

    fix: adding refactor script start

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const Server_1 = __importDefault(require("./shared/models/Server"));
+dotenv_1.default.config();
+const server = new Server_1.default();
+server.listen();
 //# sourceMappingURL=index.js.map
