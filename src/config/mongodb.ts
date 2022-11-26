d2e9af6da5019f478c87a063196641d675d4b49b
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2022-11-26 08:45:19 -0500

    fix: ajuste de direccion de db

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-  try {
-    //TODO: remover la url mongo local
-    const con = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/oci');
-    console.log(`DB connected with HOST: ${con.connection.host} DB: ${con.connection.db.namespace}`);
-  } catch (error) {
-    console.log(error);
-    throw new Error(`DB offline`);
-  }
+	try {
+		//TODO: remover la url mongo local
+		const con = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/oci');
+		console.log(`DB connected with HOST: ${con.connection.host} DB: ${con.connection.db.namespace}`);
+	} catch (error) {
+		console.log(error);
+		throw new Error(`DB offline`);
+	}
 };
